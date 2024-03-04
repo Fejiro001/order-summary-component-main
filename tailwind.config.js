6b6bd94fd7e0d6072d1cb9d3077cfeede5b13ad6
Abere Fejiro <aberefejiro@gmail.com>
2024-03-04 18:27:31 +0100

    fixed some issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "pale-blue": "hsl(225, 100%, 94%)",
         "bright-blue": "hsl(245, 75%, 52%)",
         "very-pale-blue": "hsl(225, 100%, 98%)",
-        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "desaturated-blue": "hsl(224, 23%, 35%)",
         "dark-blue": "hsl(223, 47%, 23%)",
       },
 
