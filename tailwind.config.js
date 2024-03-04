397fcbacc15ceab8b3ec406cd62d6d5db812ec06
Abere Fejiro <aberefejiro@gmail.com>
2024-03-04 17:36:38 +0100

    done styling component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)",
+      },
+
+      fontFamily: {
+        primary: ["Red Hat Display", "sans-serif"]
+      },
+
+      fontSize: {
+        heading1: "clamp(1.4rem, 2vw, 3rem)",
+        basetext: "clamp(1rem, 1.1vw, 2.5rem)",
+        smalltext: "clamp(0.875rem, 1.1vw, 2rem)"
+      }
+    },
   },
   plugins: [],
-}
+};
 
